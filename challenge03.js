986b08a2a02e3a46e3db060221c6e73940e311a0
moises <moises.huaringa>
2022-01-03 13:23:40 -0500

    add challenge #03

--- challenge03.js
+++ challenge03.js
@@ -1,44 +1,27 @@
 /*
-Te ha llegado una carta ✉️ con todos los regalos que debes preparar. El tema es que es una cadena de texto y es muy difícil de leer 😱. ¡Menos mal que han puesto cada regalo separado por espacio! (aunque ten cuidado, porque al ser niños, igual han colado más espacios de la cuenta)
-
-Encima nos hemos dado cuenta que algunas palabras vienen con un _ delante de la palabra, por ejemplo _playstation, que significa que está tachado y no se tiene que contar.
-
-Transforma el texto a un objeto que contenga el nombre de cada regalo y las veces que aparece. Por ejemplo, si tenemos el texto:
- */
-
-const letter = 'bici coche balón _playstation bici          coche peluche'
-
-const listGifts = (letter) => {
-  const invalidGiftSuffix = '_'
-	const allGifts = letter.split(' ').filter(item => !!item)
-  const giftsAvailable = allGifts.filter(item => item.charAt(0) !== invalidGiftSuffix)
-
-  let gifts = []
-  giftsAvailable.forEach(name => {
-    const gift = gifts.find(item => item.name === name)
-    if(!gift) {
-      gifts.push({ name, quantity: 1 })
-      return
-    }
-
-    gifts = gifts.map(({ name, quantity }) => ({
-      name,
-      quantity: name === gift.name ? ++quantity : quantity
-    }))
-  })
-
-  return gifts.reduce((items, item) => ({
-    ...items,
-    [item.name]: item.quantity
-  }), {})
-}
-
-console.info(listGifts(letter))
-/*
-{
-  bici: 2,
-  coche: 2,
-  balón: 1,
-  peluche: 1
-}
+  El Grinch está abriendo las cartas que iban a Santa Claus y las está dejando hechas un lío. 😱
+  Las cartas son una cadena de texto que incluyen regalos y paréntesis ().
+  Para saber si una carta es válida ✅, debes comprobar que los paréntesis cierran correctamente y que, además, no vayan vacíos.
+  ¡Pero ojo! Porque el Grinch ha dejado llaves { y corchetes [ dentro de los paréntesis que hacen que no sean válidas. Por suerte sólo los ha dejado en medio de los paréntesis...
+  Crea una función que pasándole el texto de la carta, devuelva true si es válida y false si no lo es. ¡Y acaba con la travesura del Grinch!
 */
+
+const letters = [
+  'bici coche (balón) bici coche peluche', // ✅
+  '(muñeca) consola bici', // ✅
+  'bici coche (balón bici coche', // ❌
+  'peluche (bici [coche) bici coche balón', // ❌
+  '(peluche {) bici', // ❌
+  '(a() bici (a)', // ❌,
+  '(a) (b) (c)' // ✅
+]
+
+const isValid = (letter) => {
+  const gifts = letter.split(' ').filter(item => !!item)
+  const firstValidGiftFound = gifts.find(item => /^\(.*\)$/i.test(item))
+  return /^\([a-zñáéíóú]{1,}\)$/i.test(firstValidGiftFound)
+}
+
+letters.forEach((item, index) => {
+  console.info(item, isValid(letters[index]) ? '✅' : '❌')
+})
